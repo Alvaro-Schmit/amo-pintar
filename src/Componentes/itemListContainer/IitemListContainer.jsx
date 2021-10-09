ab1722f2a199c34c702ced5aa895999300ec795d
Alvaro Schmit <alvaroschmit@gmail.com>
2021-10-08 22:00:38 -0300

    comenzando a usar firebase

--- src/Componentes/itemListContainer/IitemListContainer.jsx
+++ src/Componentes/itemListContainer/IitemListContainer.jsx
@@ -1,21 +1,22 @@
 import {useState, useEffect} from 'react';
 import ItemList from '../itemList/ItemList';
 import "./itemListContainer.css"
-import dataItems from '../../dataItems/DataItems'
+// import dataItems from '../../dataItems/DataItems'
 import Loader from '../loader/Loader'
 import { useParams } from 'react-router';
+import { getFirestore } from '../../services/getFirebase';
 
-const promise = new Promise((resolve, reject)=>{
-    const status = 200
-    if(status===200){
-        setTimeout(() => {
+// const promise = new Promise((resolve, reject)=>{
+//     const status = 200
+//     if(status===200){
+//         setTimeout(() => {
             
-            resolve(dataItems)
-        }, 2000);
-    }else{
-          reject(console.log("Algo salio mal al cargar los datos"))
-    }
-})
+//             resolve(dataItems)
+//         }, 2000);
+//     }else{
+//           reject(console.log("Algo salio mal al cargar los datos"))
+//     }
+// })
 
 function ItemListContainer () {
     
@@ -25,26 +26,59 @@ function ItemListContainer () {
 
 
     useEffect(() => {
-       
-        if (categoryId === undefined) {
-     
-        promise
-        .then(resp => setData(resp))
+
+        if(categoryId) {
+
+        const dbquery = getFirestore()
+
+        dbquery.collection('data').where('category', '==', categoryId).get()
+
+        .then(resp=>{
+            setData(resp.docs.map(dataOne => ({id: dataOne.id, ...dataOne.data()})))
+           
+            console.log(resp)})
         .catch(err => console.log(err))
         .finally(()=>setloading(false))
+
         }
-        else{
-            promise
-            .then(resp => setData(resp.filter(it=> it.category===categoryId)))
+
+        else {
+            const dbquery = getFirestore()
+
+            dbquery.collection('data').get()
+    
+            .then(resp=>{
+                setData(resp.docs.map(dataOne => ({id: dataOne.id, ...dataOne.data()})))
+               
+                console.log(resp)})
             .catch(err => console.log(err))
             .finally(()=>setloading(false))
+
         }
+
+       
+
+        
+        // dbquery.collection('data').doc('6JIruyFli7OTmDkjsKUj').get()
+        // if (categoryId === undefined) {
+     
+        // promise
+        // .then(resp => setData(resp))
+        // .catch(err => console.log(err))
+        // .finally(()=>setloading(false))
+        // }
+        // else{
+        //     promise
+        //     .then(resp => setData(resp.filter(it=> it.category===categoryId)))
+        //     .catch(err => console.log(err))
+        //     .finally(()=>setloading(false))
+        // }
 
         
    }, [categoryId])
 
 
-
+console.log(data);
     
     return(
   
